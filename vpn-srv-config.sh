be05d2f053ed48ddac1a9b94af4e3e5225fdc7a1
rinxster <sark@bk.ru>
2022-06-29 17:44:27 +0300

    Create vpn-srv-config.sh

---
+++ vpn-srv-config.sh
@@ -0,0 +1,27 @@
+# !/bin/bash
+#install updates and required programs
+yes | apt update
+yes | apt upgrade
+yes| apt install mc && apt install fail2ban
+#configure fail2ban
+sudo cp /etc/fail2ban/jail.{conf,local}
+sed -i -e 's/bantime  = 10m/bantime  = 1d/g' /etc/fail2ban/jail.local
+sudo systemctl restart fail2ban
+systemctl status fail2ban
+#configure and start ufw
+yes | apt install ufw
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
+sudo ufw allow ssh
+sudo ufw allow 22
+sudo ufw allow 1194
+yes | sudo ufw enable
+sudo ufw status verbose
+
+# pause to see intermediate result
+read -n1 -s -r -p $'Press space to continue...\n' key
+
+#download and install openvpn on server
+wget https://git.io/vpn -O openvpn-install.sh
+sudo chmod +x openvpn-install.sh
+sudo bash openvpn-install.sh
