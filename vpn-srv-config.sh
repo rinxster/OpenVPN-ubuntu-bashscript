ff2fcd47faa959dd900e8c9ab07f81c492a119e2
rinxster <sark@bk.ru>
2022-07-07 20:22:08 +0300

    Update vpn-srv-config.sh

--- vpn-srv-config.sh
+++ vpn-srv-config.sh
@@ -9,13 +9,13 @@ yes | sudo apt install unattended-upgrades
 yes | sudo apt install update-notifier-common
 sudo echo -e "APT::Periodic::Update-Package-Lists \"1\";\nAPT::Periodic::Unattended-Upgrade \"1\";\n" > /etc/apt/apt.conf.d/20auto-upgrades
 sudo systemctl restart unattended-upgrades
-sudo systemctl status unattended-upgrades
+#sudo systemctl status unattended-upgrades
 
 #configure fail2ban
 sudo cp /etc/fail2ban/jail.{conf,local}
 sudo sed -i -e 's/bantime  = 10m/bantime  = 1d/g' /etc/fail2ban/jail.local
 sudo systemctl restart fail2ban
-sudo systemctl status fail2ban
+#sudo systemctl status fail2ban
 
 #configure and start ufw
 yes | sudo apt install ufw
@@ -25,7 +25,7 @@ sudo ufw allow ssh
 sudo ufw allow 22
 sudo ufw allow 1194
 yes | sudo ufw enable
-sudo ufw status verbose
+#sudo ufw status verbose
 
 # pause to see intermediate result. Everything is ready to proceed with OpenVPN installlation
 read -n1 -s -r -p $'Everything is ready to proceed with OpenVPN installlation. Press space to continue...\n' key
