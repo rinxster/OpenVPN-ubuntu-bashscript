ace0078e193d4cb0cda90725a16679093ba5e9c9
rinxster <sark@bk.ru>
2022-08-10 11:21:15 +0300

    Update vpn-srv-config.sh
    Refactoring and also added root password change in the beginning

--- vpn-srv-config.sh
+++ vpn-srv-config.sh
@@ -1,4 +1,10 @@
 # !/bin/bash
+
+echo '########################################'
+echo 'Change your default root password!'
+echo '########################################'
+sudo passwd root
+
 #install updates and required programs
 yes | sudo apt update && yes | sudo apt upgrade
 yes | sudo apt install mc && yes | apt install fail2ban && yes | apt install curl && yes | apt install speedtest-cli && sudo apt install ufw
@@ -31,20 +37,26 @@ sed -i '/GRUB_CMDLINE_LINUX/ s/"$/ ipv6.disable=1"/' /etc/default/grub
 sudo update-grub
 
 # pause to see intermediate result.
+echo '########################################'
 read -n1 -s -r -p $'Everything is ready to change port to 2222. Press space to continue...\n' key
+echo '########################################'
 sudo ufw allow 2222 && sudo sed -i -e 's/#Port 22/Port 2222/g' /etc/ssh/sshd_config && service sshd reload && sudo ufw status
 
 # pause to see intermediate result. non root user setup and disable root login
+echo '########################################'
 read -n1 -s -r -p $'Everything is ready to proceed with non root user setup. Press space to continue...\n' key
+echo '########################################'
 nonroot=0dmin4eg && sudo useradd -m -c "$nonroot" $nonroot -s /bin/bash && usermod -aG sudo $nonroot && sudo passwd $nonroot && sudo sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config && sudo service ssh reload
 
 # pause to see intermediate result. Everything is ready to proceed with OpenVPN installlation
+echo '########################################'
 read -n1 -s -r -p $'Everything is ready to proceed with OpenVPN installlation. Press space to continue...\n' key
-
+echo '########################################'
 #download openvpn installation script and install openvpn on server
 wget https://git.io/vpn -O openvpn-install.sh && sudo chmod +x openvpn-install.sh && sudo bash openvpn-install.sh
 
 # pause to see intermediate result. Everything is ready to proceed with OpenVPN installlation
+echo '########################################'
 read -n1 -s -r -p $'Everything is ready. Press space to continue and server will restart in 1 minute. Otherwise run "shutdown -c" to cancel restart\n' key
-
+echo '########################################'
 sudo shutdown -r +1
