d181e9823f38156f679c28213245da2538b95fea
rinxster <sark@bk.ru>
2022-08-10 11:16:40 +0300

    Update vpn-srv-config.sh
    refactoring and added port 2222 and non root user creation and password change.

--- vpn-srv-config.sh
+++ vpn-srv-config.sh
@@ -1,12 +1,10 @@
 # !/bin/bash
 #install updates and required programs
-yes | sudo apt update
-yes | sudo apt upgrade
-yes | sudo apt install mc && apt install fail2ban && apt install curl && apt install speedtest-cli
+yes | sudo apt update && yes | sudo apt upgrade
+yes | sudo apt install mc && yes | apt install fail2ban && yes | apt install curl && yes | apt install speedtest-cli && sudo apt install ufw
 
 #configure and setup regular  automatic updates
-yes | sudo apt install unattended-upgrades
-yes | sudo apt install update-notifier-common
+yes | sudo apt install unattended-upgrades && yes | sudo apt install update-notifier-common
 sudo echo -e "APT::Periodic::Update-Package-Lists \"1\";\nAPT::Periodic::Unattended-Upgrade \"1\";\n" > /etc/apt/apt.conf.d/20auto-upgrades
 sudo systemctl restart unattended-upgrades
 #sudo systemctl status unattended-upgrades
@@ -28,15 +26,25 @@ sudo ufw allow 1194
 yes | sudo ufw enable
 #sudo ufw status verbose
 
+#disable ipv6 for security as not used and reboot the server
+sed -i '/GRUB_CMDLINE_LINUX/ s/"$/ ipv6.disable=1"/' /etc/default/grub
+sudo update-grub
+
+# pause to see intermediate result.
+read -n1 -s -r -p $'Everything is ready to change port to 2222. Press space to continue...\n' key
+sudo ufw allow 2222 && sudo sed -i -e 's/#Port 22/Port 2222/g' /etc/ssh/sshd_config && service sshd reload && sudo ufw status
+
+# pause to see intermediate result. non root user setup and disable root login
+read -n1 -s -r -p $'Everything is ready to proceed with non root user setup. Press space to continue...\n' key
+nonroot=0dmin4eg && sudo useradd -m -c "$nonroot" $nonroot -s /bin/bash && usermod -aG sudo $nonroot && sudo passwd $nonroot && sudo sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config && sudo service ssh reload
+
 # pause to see intermediate result. Everything is ready to proceed with OpenVPN installlation
 read -n1 -s -r -p $'Everything is ready to proceed with OpenVPN installlation. Press space to continue...\n' key
 
 #download openvpn installation script and install openvpn on server
-wget https://git.io/vpn -O openvpn-install.sh
-sudo chmod +x openvpn-install.sh
-sudo bash openvpn-install.sh
+wget https://git.io/vpn -O openvpn-install.sh && sudo chmod +x openvpn-install.sh && sudo bash openvpn-install.sh
 
-#disable ipv6 for security as not used and reboot the server
-sed -i '/GRUB_CMDLINE_LINUX/ s/"$/ ipv6.disable=1"/' /etc/default/grub
-sudo update-grub
+# pause to see intermediate result. Everything is ready to proceed with OpenVPN installlation
+read -n1 -s -r -p $'Everything is ready. Press space to continue and server will restart in 1 minute. Otherwise run "shutdown -c" to cancel restart\n' key
+
 sudo shutdown -r +1
